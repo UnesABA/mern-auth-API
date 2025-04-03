13e1e448c9f58f2b0dda02f55a8192f3f09843f7
UnesABA <unesaitbenali@gmail.com>
2025-04-03 14:01:02 +0000

    Updating mern-auth-boilerplate

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-// express → A lightweight web framework for handling API requests and routes.
-
-// mongoose → Helps interact with MongoDB, making it easier to model and manage data.
-
-// dotenv → Loads environment variables from a .env file (e.g., database URLs, API keys).
-
-// helmet → Adds security headers to protect your app from common vulnerabilities.
-
-// morgan → Logs HTTP requests in the terminal for debugging.
-
-// nodemon → Automatically restarts the server when code changes (only used in development).
-
 const express   = require("express")
 const app       = express()
 const mongoose  = require("mongoose")
 const dotenv    = require("dotenv")
 const helmet    = require("helmet")
 const morgan    = require("morgan")
+const cors      = require("cors")
 const userRoute = require("./routes/users.js")
 const authRoute = require("./routes/auth.js")
 const postRoute = require("./routes/posts.js")
@@ -30,22 +19,13 @@ mongoose.connect(process.env.MONGO_URL)
 })
 .catch((err) => console.error('Error connecting to MongoDB:', err))
 
-//middleware
+// Middleware
 app.use(express.json())
 app.use(helmet())
 app.use(morgan("common"))
+app.use(cors())
 
+// Routes
 app.use("/api/users", userRoute)
 app.use("/api/auth" , authRoute)
 app.use("/api/posts", postRoute)
-
-// app.get("/", (req, res) =>{
-//   res.send("Welcome to Homepage !")
-// })
-// app.get("/users", (req, res) =>{
-//   res.send("Welcome to user page !")
-// })
-
-app.listen(8800, () =>{
-  console.log("The backend server is Running !")
-})
--- routes/users.js
+++ routes/users.js
@@ -41,14 +41,18 @@ router.delete("/:id", async(req, res) =>{
 })
 
 //get a user
-router.get("/:id", async(req, res) =>{
-    try {
-      const user = await User.findById(req.params.id)
-      const {password, updatedAt, ...other} = user._doc
-      res.status(200).json(other)
-    } catch (error) {
-      return res.status(500).json(error)
-    }
+router.get("/", async (req, res) => {
+  const userId = req.query.userId
+  const username = req.query.username
+  try {
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username })
+    const { password, updatedAt, ...other } = user._doc
+    res.status(200).json(other)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 //follow user
